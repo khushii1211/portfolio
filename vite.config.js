194d2df78e7034877bb158f2a1ce516d984b531a
khushii1211 <khushii1211@users.noreply.github.com>
2026-07-13 21:47:57 +0530

    Configure Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'assets',
-  base: '/khushi-model-portfolio/',
+  base: '/',
   server: {
     proxy: { '/api': 'http://localhost:3001' },
     watch: { ignored: ['**/.chrome-*/**'] },
